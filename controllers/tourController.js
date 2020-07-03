a9cd3b48111e2dd69c858b0a93eeff0e7104d28c
John-Wambua <jwambua307@gmail.com>
2020-07-03 15:39:41 +0300

    API sorting and pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,7 +3,31 @@ const { Tour ,validate}=require('../models/tour');
 
 exports.getAllTours=(req,res)=>{
 
-  Tour.find({},(err,tours)=>{
+  const queryObj= { ...req.query };
+  const excludedFields=['page','sort','limit','fields'];
+
+  excludedFields.forEach(el=>{
+    delete queryObj[el];
+  });
+  // console.log(queryObj);
+//{ difficulty: 'difficult', duration: { gte: '4' } }
+  let queryStr=JSON.stringify(queryObj)
+  queryStr=queryStr.replace(/\b(gte|gt|lte|lt)\b/g,match=>`$${match}`)
+  // console.log(JSON.parse(queryStr));
+//{ difficulty: 'difficult', duration: { '$gte': '4' } }
+
+  let query=Tour.find(JSON.parse(queryStr));
+
+  if (req.query.sort){
+    //Allow sorting by multiple parameters separated by comma
+    const sortBy=req.query.sort.split(',').join(' ');
+    console.log(sortBy);
+    query=query.sort(sortBy );
+    //sort('price duration')
+  }else{
+    query=query.sort({createdAt:-1});
+  }
+  query.exec((err,tours)=>{
     if (err) return res.status(500).json({
       status:"failed",
       message:err
