6a6b7a88724d204c3130ced595f7f46196ae1b1c
John-Wambua <jwambua307@gmail.com>
2020-07-13 13:36:43 +0300

    api security

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express=require('express');
 const morgan=require('morgan');
+const rateLimit=require('express-rate-limit');
+const helmet=require('helmet')
+const mongoSanitize = require('express-mongo-sanitize');
+const xss=require('xss-clean');
+const hpp=require('hpp');
 
 const AppError=require('./utils/appError')
 const globalErrorHandler=require('./middleware/error');
@@ -10,10 +15,40 @@ const app=express();
 
 
 //MIDDLEWARE
+//Set HTTP security headers
+app.use(helmet());
+
+//Development logging
+if(process.env.NODE_ENV==='development'){
+  app.use(morgan('dev'));
+}
+
+//Limit requests from same IP
+const apiLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 15 minutes
+  max: 100,
+  message:'Too many requests from this IP, please try again in an hour!'
+});
+
+// only apply to requests that begin with /api/
+app.use("/api/", apiLimiter);
+
 app.use(express.static(`${__dirname}/public`));
-app.use(express.urlencoded({extended:true}))
-app.use(morgan('dev'));
-app.use(express.json());
+//Body-parsing
+app.use(express.urlencoded({extended:true,limit:'10kb'}))
+//Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+//Data sanitization against XSS
+app.use(xss())
+
+//Prevent parameter pollution
+app.use(hpp({
+  whitelist:[
+    'duration','ratingsAverage','ratingQuantity','maxGroupSize','difficulty','price'
+  ]
+}));
+
 
 //ROUTES
 app.use('/api/v1/tours',tours);
--- models/user.js
+++ models/user.js
@@ -97,6 +97,12 @@ userSchema.methods.correctPassword=async function(candidatePassword,next) {
 }
 userSchema.methods.generateAuthToken=function(statusCode,res) {
   const token=jwt.sign({id:this._id},process.env.JWT_SECRET,{expiresIn:process.env.JWT_EXP});
+  const cookieOptions={
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN *24 *60*60*1000),
+    httpOnly:true
+  }
+  if (process.env.NODE_ENV==='production') cookieOptions.secure=true;
+  res.cookie('jwt',token,cookieOptions);
   return res.status(statusCode).json({
     status:'success',
     token,
